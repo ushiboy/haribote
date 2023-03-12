e41aa3f5bc9c50e98c5cf1ee69dad665bdcfcaa0
ushiboy <ushiboy.dev@gmail.com>
2023-03-12 09:52:38 +0900

    add proxy setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/haribote/",
-  server: {
-    port: 3000,
-  },
-  plugins: [react(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const proxy = env.VITE_PROXY_TARGET
+    ? {
+        "/haribote/api": {
+          target: env.VITE_PROXY_TARGET,
+          changeOrigin: true,
+        },
+      }
+    : undefined;
+  return {
+    base: "/haribote/",
+    server: {
+      port: 3000,
+      proxy,
+    },
+    plugins: [react(), tsconfigPaths()],
+  };
 });
