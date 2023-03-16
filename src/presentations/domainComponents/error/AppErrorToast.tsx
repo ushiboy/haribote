25764720f81336ef404c1dc552e718d13eade714
ushiboy <ushiboy.dev@gmail.com>
2023-03-16 21:36:57 +0900

    セッション切れのときに現在のパスをリダイレクトURLに格納

--- src/presentations/domainComponents/error/AppErrorToast.tsx
+++ src/presentations/domainComponents/error/AppErrorToast.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate } from "react-router";
+import { Navigate, useLocation } from "react-router";
 
 import { ApplicationException, WebApiException } from "@/domains/errors";
 import { useErrorMessage } from "@/presentations/hooks/shared";
@@ -13,13 +13,19 @@ export const AppErrorToast: React.FC<{
   duration?: number;
   onClose?: () => void;
 }> = ({ error, duration, onClose }) => {
+  const location = useLocation();
   const { toMessageFromError } = useErrorMessage();
   if (!error) {
     return null;
   }
   if (error instanceof WebApiException && error.statusCode === 401) {
     // セッション切れの場合ログインに飛ばす
-    return <Navigate to="/" replace />;
+    return (
+      <Navigate
+        to={`/?redirectUrl=${encodeURIComponent(location.pathname)}`}
+        replace
+      />
+    );
   }
   return (
     <ErrorToast
